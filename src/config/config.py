891e4a7ba2f85a3600799019b9c9ed148c3fe6e8
Daniel-615 <suyanangel615@gmail.com>
2025-05-02 17:50:13 -0600

    Corregido confusión en importaciones.

--- src/config/config.py
+++ src/config/config.py
@@ -1,68 +1,87 @@
-from src.lab.controllers.cliente import Cliente
-from src.lab.controllers.asunto import Asunto
-from src.lab.controllers.abogado import Abogado
-from src.lab.controllers.gabinete import Gabinete   
-from src.lab.controllers.abogado_gabinete import AbogadoGabinete
-from src.lab.controllers.audiencia import Audiencia 
-from src.lab.controllers.incidencia import Incidencia
-from src.lab.controllers.log_asunto import LogAsunto
+# CONTROLADORES
+from src.lab.controllers.cliente import Cliente as ClienteController
+from src.lab.controllers.asunto import Asunto as AsuntoController
+from src.lab.controllers.abogado import Abogado as AbogadoController
+from src.lab.controllers.gabinete import Gabinete as GabineteController
+from src.lab.controllers.abogado_gabinete import AbogadoGabinete as AbogadoGabineteController
+from src.lab.controllers.audiencia import Audiencia as AudienciaController
+from src.lab.controllers.incidencia import Incidencia as IncidenciaController
+from src.lab.controllers.log_asunto import LogAsunto as LogAsuntoController
 
-from src.lab.routes.cliente import Cliente
-from src.lab.routes.asunto import Asunto
-from src.lab.routes.abogado import Abogado
-from src.lab.routes.gabinete import Gabinete   
-from src.lab.routes.abogado_gabinete import AbogadoGabinete
-from src.lab.routes.audiencia import Audiencia 
-from src.lab.routes.incidencia import Incidencia
-from src.lab.routes.log_asunto import LogAsunto
+# RUTAS
+from src.lab.routes.cliente import Cliente as ClienteRoute
+from src.lab.routes.asunto import Asunto as AsuntoRoute
+from src.lab.routes.abogado import Abogado as AbogadoRoute
+from src.lab.routes.gabinete import Gabinete as GabineteRoute
+from src.lab.routes.abogado_gabinete import AbogadoGabinete as AbogadoGabineteRoute
+from src.lab.routes.audiencia import Audiencia as AudienciaRoute
+from src.lab.routes.incidencia import Incidencia as IncidenciaRoute
+from src.lab.routes.log_asunto import LogAsunto as LogAsuntoRoute
 
 class Config:
-    def __init__(self,app,db,models):
-        self.app=app
-        self.db=db
-        self.models=models
-        self.controllers_oracle(db,models)
-        self.controllers_sql_server(db,models)
+    def __init__(self, app, db, models):
+        self.app = app
+        self.db = db
+        self.models = models
+
+        self.controllers_oracle(db, models)
+        self.controllers_sql_server(db, models)
+
         self.routes_oracle()
         self.routes_sql_server()
-    #Geters
+
+    # Getters
     def getModels(self):
         return self.models
-    #SQL SERVER
+
+    # SQL SERVER: Getters para controladores
     def getClienteControllers(self):
         return self.cliente
+
     def getAsuntoControllers(self):
         return self.asunto
+
     def getAbogadoControllers(self):
         return self.abogado
+
     def getGabineteControllers(self):
         return self.gabinete
+
     def getAbogadoGabineteControllers(self):
         return self.abogado_gabinete
+
     def getAudienciaControllers(self):
         return self.audiencia
+
     def getIncidenciaControllers(self):
         return self.incidencia
-    #Oracle
+
+    # Oracle: Getter
     def getLogAsuntoControllers(self):
         return self.log_asunto
-    def controllers_sql_server(self,db,models):
-        self.cliente=Cliente(db,models)
-        self.asunto=Asunto(db,models)
-        self.abogado=Abogado(db,models)
-        self.gabinete=Gabinete(db,models)   
-        self.abogado_gabinete=AbogadoGabinete(db,models)
-        self.audiencia=Audiencia(db,models) 
-        self.incidencia=Incidencia(db,models)
-    def controllers_oracle(self,db,models):
-        self.log_asunto=LogAsunto(db,models)
+
+    # INSTANCIACIÓN DE CONTROLADORES (lógica de negocio)
+    def controllers_sql_server(self, db, models):
+        self.cliente = ClienteController(db, models)
+        self.asunto = AsuntoController(db, models)
+        self.abogado = AbogadoController(db, models)
+        self.gabinete = GabineteController(db, models)
+        self.abogado_gabinete = AbogadoGabineteController(db, models)
+        self.audiencia = AudienciaController(db, models)
+        self.incidencia = IncidenciaController(db, models)
+
+    def controllers_oracle(self, db, models):
+        self.log_asunto = LogAsuntoController(db, models)
+
+    # REGISTRO DE RUTAS (API)
     def routes_sql_server(self):
-        self.cliente=Cliente(self.app,self)
-        self.asunto=Asunto(self.app,self)
-        self.abogado=Abogado(self.app,self)
-        self.gabinete=Gabinete(self.app,self)
-        self.abogado_gabinete=AbogadoGabinete(self.app,self)
-        self.audiencia=Audiencia(self.app,self)
-        self.incidencia=Incidencia(self.app,self)
+        self.cliente_route = ClienteRoute(self.app, self)
+        self.asunto_route = AsuntoRoute(self.app, self)
+        self.abogado_route = AbogadoRoute(self.app, self)
+        self.gabinete_route = GabineteRoute(self.app, self)
+        self.abogado_gabinete_route = AbogadoGabineteRoute(self.app, self)
+        self.audiencia_route = AudienciaRoute(self.app, self)
+        self.incidencia_route = IncidenciaRoute(self.app, self)
+
     def routes_oracle(self):
-        self.log_asunto=LogAsunto(self.app,self)
+        self.log_asunto_route = LogAsuntoRoute(self.app, self)
