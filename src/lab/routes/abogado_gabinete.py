fd4d5883e114bfdcd5d37cd248ad65f4ab8e5aac
Daniel-615 <suyanangel615@gmail.com>
2025-05-02 14:24:04 -0600

    route for abogado gabinete

--- src/lab/routes/abogado_gabinete.py
+++ src/lab/routes/abogado_gabinete.py
@@ -1,7 +1,25 @@
+from flask import request
 class AbogadoGabinete:
     def __init__(self,app,app_initializer):
         self.app=app
         self.app_initializer=app_initializer
         self.routes()
     def routes(self):
-        pass
+        @self.app.route('/v1/abogado/gabinete',methods=['GET'])
+        def get_abogado_gabinete():
+            """
+            Get all abogado_gabinete
+            """
+            return self.app_initializer.getAbogadoGabineteControllers().get_abogado_gabinete()
+        @self.app.route('/v1/abogado/gabinete/<int:id>',methods=['GET'])
+        def get_abogado_gabinete_by_id(id):
+            """
+            Get abogado_gabinete by id
+            """
+            return self.app_initializer.getAbogadoGabineteControllers().get_abogado_gabinete_by_id(id)
+        @self.app.route('/v1/abogado/gabinete',methods=['POST'])
+        def create_abogado_gabinete():
+            """
+            Create abogado_gabinete
+            """
+            return self.app_initializer.getAbogadoGabineteControllers().create_abogado_gabinete(request.json)
